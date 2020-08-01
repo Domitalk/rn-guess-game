4055bdddee6e7b313ddbaa428a87233331b144de
domitalk <dominic.m.chu@gmail.com>
2020-08-01 16:19:38 -0400

    confirmInputHandler

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -8,10 +8,27 @@ import Input from '../components/Input'
 const StartGameScreen = props => {
 
     const [enteredValue, setEnteredValue] = useState('')
+    const [confirmed, setConfirmed] = useState(false)
+    const [selectedNumber, setSelectedNumber] = useState()
 
     const numberInputHandler = inputText => {
         // anything not a number is not allowed 
         setEnteredValue(inputText.replace(/[^0-9]/g, ''))
+    }
+
+    const resetInputHandler = () => {
+        setEnteredValue('')
+        setConfirmed(false)
+    }
+
+    const confirmInputHandler = () => {
+        const chosenNumber = parseInt(enteredValue); 
+        if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+            return; 
+        }
+        setConfirmed(true);
+        setEnteredValue('');
+        setSelectedNumber(chosenNumber)
     }
 
     return (
@@ -37,14 +54,14 @@ const StartGameScreen = props => {
                         <View style={styles.button}>
                             <Button 
                                 title="Reset" 
-                                onPress={() => {}} 
+                                onPress={resetInputHandler} 
                                 color={Colors.accent}
                             /> 
                         </View>
                         <View style={styles.button}>
                             <Button 
                                 title="Confirm" 
-                                onPress={() => {}} 
+                                onPress={confirmInputHandler} 
                                 color={Colors.primary}
                             /> 
                         </View>
