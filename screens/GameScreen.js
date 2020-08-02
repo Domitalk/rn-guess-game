a7c8342f00062c121aa6f07639a9714ee44c4cec
domitalk <dominic.m.chu@gmail.com>
2020-08-02 11:18:40 -0400

    handled conditional render logic for screens

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Button, Text, StyleSheet } from 'react-native';
 
+import NumberContainer from '../components/NumberContainer'
+import Card from '../components/Card'
 
 // shouldn't make at every rerender so outside of component 
 const generateRandomBetween = (min, max, exclude) => {
@@ -19,15 +21,35 @@ const GameScreen = props => {
         generateRandomBetween(1, 100, props.userChoice)
     );
 
-    return (
-        <View>
+    const nextGuessHandler = direction => {
+        
+    }
 
+    return (
+        <View style={styles.screen}>
+            <Text>Opponent's Guess</Text>
+            <NumberContainer>{currentGuess}</NumberContainer>
+            <Card style={styles.buttonContainer}>
+                <Button title="LOWER" onPress={() => {}} />
+                <Button title="GREATER" onPress={() => {} } />
+            </Card>
         </View>
     )
 }; 
 
 const styles = StyleSheet.create({
-
+    screen: {
+        flex: 1, 
+        padding: 10,
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 20,
+        width: 300,
+        maxWidth: '80%'
+    }
 });
 
 export default GameScreen;
